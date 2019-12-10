d6b12fc27e6b4bcdedb5c7ea115921b9ab0cc6d9
unknown <you@example.com>
2019-12-10 09:01:33 +0100

    Data generation changes

---
+++ data_generation/create_company_data.py
@@ -0,0 +1,45 @@
+import os
+import time
+import glob
+import argparse
+import subprocess
+from queue import Queue, Empty
+from threading import Thread
+
+
+def enqueue_output(out, queue):
+    for line in iter(out.readline, b''):
+        queue.put(line)
+    out.close()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Generate company data')
+    parser.add_argument('path', type=str, help='Folder where configurations files live')
+    args = parser.parse_args()
+
+    q = Queue()
+    processes = []
+    for f in glob.glob(os.path.join(args.path, '*.cfg')):
+        p = subprocess.Popen(['python', 'create_facility_data.py', f], stdout=subprocess.PIPE)
+        processes.append(p)
+        t = Thread(target=enqueue_output, args=(p.stdout, q))
+        t.daemon = True  # thread dies with the program
+        t.start()
+
+    # Print info
+    while True:
+        finished_counter = 0
+        for p in processes:
+            if p.poll() is not None: # Not finished
+                finished_counter += 1
+        try:
+            while True:
+                print(q.get_nowait().decode(), end='')
+        except Empty:
+            pass
+
+        if finished_counter == len(processes):
+            break
+        time.sleep(1)
---
+++ data_generation/create_facility_data.py
@@ -0,0 +1,70 @@
+import os
+import shutil
+import configparser
+import argparse
+
+import uuid
+import time
+from data_generation.simulation import Clock, Facility, Machine
+from data_generation.notify import CsvNotifier
+
+
+def create_facility(configuration):
+    fac = Facility(**configuration)
+    for _ in range(int(configuration['machine_count'])):
+        machine_id = str(uuid.uuid4())
+        telemetry_notifier = CsvNotifier(configuration['telemetry_path'], 'telemetry_%s' % machine_id, buffer_size=None)
+        event_notifier = CsvNotifier(configuration['event_path'], 'event_%s' % machine_id, buffer_size=None)
+        fac.add(
+            Machine(name=machine_id, telemetry_notifier=telemetry_notifier, event_notifier=event_notifier,
+                    **configuration))
+    return fac
+
+
+if __name__ == '__main__':
+
+    start_time = time.time()
+
+    parser = argparse.ArgumentParser(description='Generate a facility dataset')
+    parser.add_argument('config', type=str, help='Facility configuration file')
+    arg = parser.parse_args()
+    config = configparser.ConfigParser()
+    config.read(arg.config)
+    cl = Clock()
+    facility_name = config['INFO']['name']
+    duration = config.getint('INFO', 'simulation_time')
+    config_dict = {'clock': cl}
+
+    for key in config['CONFIGURATION']:
+        try:
+            config_dict[key] = config.getfloat('CONFIGURATION', key)
+        except ValueError as e:
+            config_dict[key] = config.get('CONFIGURATION', key)
+
+    facility = create_facility(config_dict)
+
+    paths = (config_dict['telemetry_path'], config_dict['event_path'])
+
+    # Create dirs structure
+
+    for path in paths:
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        os.makedirs(path)
+
+    progress_step = int(duration / 10)
+
+    for i in range(duration):
+        if i % progress_step == 0:
+            print('Generating data for "%s" ... %.2f%%' % (facility_name, 100. * i / duration))
+        next(facility)
+        next(cl)
+    print('Generating data for "%s" ... %.2f%%' % (facility_name, 100))
+
+    print('Saving files for "%s" ... ' % facility_name)
+
+    for machine in facility.machines:
+        machine.telemetry_notifier.flush()
+        machine.event_notifier.flush()
+
+    print('All done for "%s". Task finished in %.2f seconds' % (facility_name, time.time() - start_time))
